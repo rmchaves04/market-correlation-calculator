9c345a3178adac23ce62381cd3049e3914544d87
Rodrigo Chaves <119129725+rmchaves04@users.noreply.github.com>
2024-11-21 14:26:27 -0300

    fix calculations (use pct change)

--- data.py
+++ data.py
@@ -1,7 +1,23 @@
+import os
+import pandas as pd
 import yfinance as yf
 
 def download_data(symbols):
     data = {}
     for symbol in symbols:
-        data[symbol] = yf.download(symbol)['Close']
+        data[symbol] = yf.download(symbol)['Close'].pct_change().dropna()
     return data
+
+def import_csv():
+    files = os.listdir('data')
+    df = pd.DataFrame()
+
+    for file in files:
+        if file.endswith('.csv'):
+            name = file.split('.')[0]
+            data = pd.read_csv(f'data/{file}', index_col=0).dropna()
+            data.columns = [name]
+            df = pd.concat([df, data], axis=1)
+
+    df = df.dropna()
+    return df
--- main.py
+++ main.py
@@ -1,17 +1,35 @@
+import os
 import pandas as pd
 from workbook import save_to_workbook
-from data import download_data
+from data import download_data, import_csv
 
-with open("symbols.txt") as f:
-    symbols = f.read().splitlines()
+def yahoo():
+    with open("symbols.txt") as f:
+        symbols = f.read().splitlines()
 
-data = download_data(symbols)
-data = pd.DataFrame(data).dropna()
+    data = download_data(symbols)
+    data = pd.DataFrame(data).dropna()
 
-starting_date = data.index[0].strftime('%Y-%m-%d')
+    starting_date = data.index[0].strftime('%Y-%m-%d')
 
-print(f'Correlation Matrix (since {starting_date}):')
-correlation_matrix = data.corr(method='pearson')
-print(correlation_matrix)
+    print(f'Correlation Matrix (since {starting_date}):')
+    correlation_matrix = data.corr(method='pearson')
+    print(correlation_matrix)
 
-save_to_workbook(correlation_matrix, starting_date)
+    save_to_workbook(correlation_matrix, starting_date)
+
+def csv():
+    data = import_csv()
+    data = data.apply(pd.to_numeric, errors='coerce')
+    data = data.dropna()
+
+    data = data.pct_change().dropna()
+    starting_date = data.index[0]
+
+    print(f'Correlation Matrix (since {starting_date}):')
+    correlation_matrix = data.corr(method='pearson')
+    print(correlation_matrix)
+
+    save_to_workbook(correlation_matrix, starting_date)
+
+yahoo()
